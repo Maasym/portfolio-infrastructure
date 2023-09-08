286ca2cc3ac6f949511339b00a3bef53cd5e74a5
tokyo <tokyo@tokyo.local>
2023-09-08 12:10:25 +0200

    change aks node count

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name            = "agentpool"
     vm_size         = "Standard_B2s"
-    node_count      = "1"
+    node_count      = "2"
     vnet_subnet_id  = azurerm_subnet.aks-subnet.id
     os_disk_size_gb = "32"
   }
